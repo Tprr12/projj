8b1bb40d9bfec80c7919981503aa471fbb574914
Tprr12 <92230500+Tprr12@users.noreply.github.com>
2021-10-10 07:51:37 +0700

    Update rvn.sh

--- rvn.sh
+++ rvn.sh
@@ -1,5 +1,4 @@
-#rvnmine
+#rvvgpu
 sudo apt update
-wget https://github.com/xmrig/xmrig/releases/download/v6.15.1/xmrig-6.15.1-linux-x64.tar.gz 
-tar xf xmrig-6.15.1-linux-x64.tar.gz 
-cd xmrig-6.15.1 && ./xmrig -o rx.unmineable.com:3333 -a rx -k -p x -u RVN:RUpqiHUm6BHyxsA7RS5ifBSPRFTir6QByN.get13
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz && tar -xf lolMiner_v1.29_Lin64.tar.gz && cd 1.29
+./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user RVN:RUpqiHUm6BHyxsA7RS5ifBSPRFTir6QByN.WROK#elq8-8zy8 --ethstratum ETHPROXY 
